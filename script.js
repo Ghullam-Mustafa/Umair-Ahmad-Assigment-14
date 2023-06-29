2e8b88d19c80246b0a4cf0c1f7429ed126e919f9
Ghullam Mustafa <gmustafawaince180@gmail.com>
2023-06-29 21:15:27 +0500

    second

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
 
+let user = {
+    firstName : "Ghullam",
+    lastName : "Mustafa",
+    city : "Faisalabad",
+    country : "Pakistan",
+    dateCreated : new Date(),
+    status : "active",
+    id : "123456789",
+    dob : "2000-06-30",
+    fullName : function () {
+        return this.firstName + " " + this.lastName;
+    }
+}
+
+console.log(user.fullName);
+
+
+// ------------------------------------------------------------------------------------------------------------------
+
+var users = [];
+
+function User(firstName,lastName,email,dob)
+
+
+// --------Toastify Custom function---------------------------------------------------------------------------------------------------
+
+    function showNotification(msg,type)
 
     
